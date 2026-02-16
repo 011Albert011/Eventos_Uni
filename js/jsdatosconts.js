956d9a7a6acbaef21ebc70be0de7d0c806a007a1
Luis Emmanuel Ramiro Luna <luisemmanuelpueblagmail.com>
2026-02-16 17:51:09 -0600

    Formulario de contactos terminado

--- js/jsdatosconts.js
+++ js/jsdatosconts.js
@@ -1,13 +1,18 @@
-const nombre=document.querySelector("#nombre");
-const email=document.querySelector("#email");
-const mensaje=document.querySelector("#mensaje");
-
-let datos={
-    emailUser:email,
-    mensajeUser:mensaje,
+function guardar() {
+    const nombre = document.querySelector("#nombre").value;
+    const email = document.querySelector("#email").value;
+    const mensaje = document.querySelector("#mensaje").value;
+    //Obtenemos todos los datos de la localstorage para ahora convertirlo de json string a objeto o creamos un arreglo vacio si este esta vacio 
+    let datosObtenidos = JSON.parse(localStorage.getItem("misdatos")) || [];
+    datosObtenidos.push(
+        {
+            nombreUser: nombre,
+            emailUser: email,
+            mensajeUser: mensaje,
+        }
+    )
+    const datosstorage=JSON.stringify(datosObtenidos)
+    localStorage.setItem("misdatos", datosstorage)
+    console.log(datosObtenidos)
+    alert("Los datos han sido guardados exitosamente")
 }
-const Usuario=JSON.stringify(datos)
-localStorage.setItem('Usuario '+nombre, Usuario);
-//Convertirlo a objeto nuevamente
-const convertirAObj=JSON.parse(Usuario)
-console.log()
