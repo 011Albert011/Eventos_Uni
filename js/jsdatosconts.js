322635089d1e332dbd20c6e47a981b003b3d084a
Luis Emmanuel Ramiro Luna <luisemmanuelpueblagmail.com>
2026-02-16 18:49:56 -0600

    Formulario de contacto agregado

--- js/jsdatosconts.js
+++ js/jsdatosconts.js
@@ -1,9 +1,12 @@
+
 function guardar() {
     const nombre = document.querySelector("#nombre").value;
     const email = document.querySelector("#email").value;
     const mensaje = document.querySelector("#mensaje").value;
+    const resultado=document.querySelector("#resultConts");
     //Obtenemos todos los datos de la localstorage para ahora convertirlo de json string a objeto o creamos un arreglo vacio si este esta vacio 
     let datosObtenidos = JSON.parse(localStorage.getItem("misdatos")) || [];
+    //Agregamos mas elementos a la lista con el metodo push
     datosObtenidos.push(
         {
             nombreUser: nombre,
@@ -11,8 +14,12 @@ function guardar() {
             mensajeUser: mensaje,
         }
     )
+    //Lo regresamos a string
     const datosstorage=JSON.stringify(datosObtenidos)
     localStorage.setItem("misdatos", datosstorage)
-    console.log(datosObtenidos)
-    alert("Los datos han sido guardados exitosamente")
+    console.log(datosObtenidos);
+    resultado.innerHTML="Los datos han sido guardados correctamente"
+}
+function buscarDato(){
+    
 }
