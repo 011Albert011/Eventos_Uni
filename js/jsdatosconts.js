87f7d73191b5af6e24bb3b5e70885f7a0797ec70
Luis Emmanuel Ramiro Luna <luisemmanuelpueblagmail.com>
2026-02-16 21:48:36 -0600

    Cambios ajustados

--- js/jsdatosconts.js
+++ js/jsdatosconts.js
@@ -2,22 +2,28 @@
 function guardar() {
     const nombre = document.querySelector("#nombre").value;
     const email = document.querySelector("#email").value;
-    const mensaje = document.querySelector("#mensaje").value;
-    //Obtenemos todos los datos de la localstorage para ahora convertirlo de json string a objeto o creamos un arreglo vacio si este esta vacio 
-    let datosObtenidos = JSON.parse(localStorage.getItem("misdatos")) || [];
-    //Agregamos mas elementos a la lista con el metodo push
-    datosObtenidos.push(
-        {
-            nombreUser: nombre,
-            emailUser: email,
-            mensajeUser: mensaje,
-        }
-    )
-    //Lo regresamos a string
-    const datosstorage = JSON.stringify(datosObtenidos)
-    localStorage.setItem("misdatos", datosstorage)
-    console.log(datosObtenidos);
-    alert("Los datos han sido guardados correctamente")
+    const mensaje = (document.querySelector("#mensaje").value).trim();
+    
+    if (nombre === '' || email === '' || mensaje === '') {
+        alert("No a completado todos los campos");
+    }
+    else {
+        //Obtenemos todos los datos de la localstorage para ahora convertirlo de json string a objeto o creamos un arreglo vacio si este esta vacio 
+        let datosObtenidos = JSON.parse(localStorage.getItem("misdatos")) || [];
+        //Agregamos mas elementos a la lista con el metodo push
+        datosObtenidos.push(
+            {
+                nombreUser: nombre,
+                emailUser: email,
+                mensajeUser: mensaje,
+            }
+        )
+        //Lo regresamos a string
+        const datosstorage = JSON.stringify(datosObtenidos)
+        localStorage.setItem("misdatos", datosstorage)
+        console.log(datosObtenidos);
+        alert("Los datos han sido guardados correctamente")
+    }
 }
 function buscarDato() {
 
